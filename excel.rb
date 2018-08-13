8fe08319852a683ce4001ab50fa22819cda845fa
日野恵太 <hinokeita@hinokeitanoMacBook-puro.local>
2018-08-13 12:46:03 +0900

    refactor:定数を

--- excel.rb
+++ excel.rb
@@ -1,11 +1,7 @@
 require 'rubyXL'
 require './Config_Reader'
 
-class Excel
-
-  #コマンドライン引数を受け取る
-  # IN_FILE_PATH = ARGV[0]
-  #本来は上記だが、開発ではこっち
+module CONF
   IN_FILE_PATH   = "/Users/hinokeita/Desktop/workspace/summary_excel/target_folder".freeze
   OUT_FILE_PATH  = "/Users/hinokeita/Desktop/workspace/summary_excel/output_file".freeze
   CONFIG_FILE    = "env.rb".freeze
@@ -13,6 +9,14 @@ class Excel
   FILE_TYPE      = ".xlsx".freeze
   IN_SHEET_NAME  = "シート1".freeze
   OUT_SHEET_NAME = "20180811".freeze
+  GID_ROW        = 1
+  PJNAME_ROW     = 2
+  ZISSEKI        = 3
+  MIKOMI         = 4
+end
+
+class Excel
+  include CONF
 
   def read
     sum_array = [{}]
@@ -27,16 +31,19 @@ class Excel
       worksheet = workbook[IN_SHEET_NAME]
 
       #ファイル内の予実を取得
-      while !(worksheet[count][2].value == nil) do
-        name = convert_gid(config,worksheet[count][1].value.to_s)
-        pj_name = worksheet[count][2].value
-        zisseki = worksheet[count][3].value
-        mikomi = worksheet[count][4].value
+      until worksheet[count][GID_ROW].value == nil do
+        name = convert_gid(config,worksheet[count][GID_ROW].value.to_s)
+        pj_name = worksheet[count][PJNAME_ROW].value
+        zisseki = worksheet[count][ZISSEKI].value
+        mikomi = worksheet[count][MIKOMI].value
         sum_array.push(
-          "#{pj_name}":[
-          "実績":zisseki,
-          "見込み":mikomi
-        ])
+          "#{name}":[
+            "#{pj_name}":[
+              "実績":zisseki,
+              "見込み":mikomi
+            ]
+          ]
+        )
         count = count + 1
       end
     end
@@ -44,9 +51,6 @@ class Excel
     #配列を初期化した際の要素を削除
     sum_array.delete_at(0)
     return sum_array
-    # puts sum_array
-
-    # puts buf[0][:"test"][0][:"実績"]
   end
 
   def convert_gid(config,gid)
@@ -68,7 +72,6 @@ class Excel
     workbook = RubyXL::Parser.parse(out_path)
     worksheet = workbook[OUT_SHEET_NAME]
     worksheet[3][2].change_contents(2.4)
-
 
     # workbook.write(out_path)
     puts worksheet[3][2].value
